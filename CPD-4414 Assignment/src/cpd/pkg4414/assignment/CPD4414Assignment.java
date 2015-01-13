656b1f8e0f8d9e3e4c2f3ba95f100e609c7a2d05
Jeff Codling <circuitjc@gmail.com>
2015-01-13 12:55:42 -0500

    Initial Commit

---
+++ CPD-4414 Assignment/src/cpd/pkg4414/assignment/CPD4414Assignment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cpd.pkg4414.assignment;
+
+/**
+ *
+ * @author Jeff Codling - c0471944 - Lambton College CPRO
+ */
+public class CPD4414Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
