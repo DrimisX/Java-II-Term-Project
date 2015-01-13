e2ac4d01af0d3d419d1cdd1d57012861288220bc
Jeff Codling <circuitjc@gmail.com>
2015-01-13 14:18:33 -0500

    End of class. Jan 13. Working on tests.

---
+++ CPD-4414 Assignment/test/cpd/pkg4414/assignment/OrderTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd.pkg4414.assignment;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jeff
+ */
+public class OrderTest {
+    
+    public OrderTest() {
+    }
+
+    @Test
+    public void testOrderNoTimeProcessedOnFullfill() {
+        System.out.println("testOrderNoTimeProcessedOnFullfill");
+        Purchase purchase = new Purchase(132,500);
+        ArrayList<Purchase> purchases = new ArrayList<>();
+        purchases.add(purchase);
+        Order order = new Order("ABC123","Sample name",Date.valueOf("2014-12-01"),null,null,purchases,"Test Order");
+        
+        // not done yet
+    }
+    
+}
---
+++ CPD-4414 Assignment/test/cpd/pkg4414/assignment/PurchaseTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd.pkg4414.assignment;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jeff
+ */
+public class PurchaseTest {
+    
+    public PurchaseTest() {
+    }
+    
+    @Test
+    public void testSomeMethod() {
+        
+    }
+    
+}
