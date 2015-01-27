e72849428987351441016228a52ae1b75c01ec66
Jeff Codling <circuitjc@gmail.com>
2015-01-27 13:42:19 -0500

    Changed Order constructor.

--- CPD-4414 Assignment/src/cpd/pkg4414/assignment/Order.java
+++ CPD-4414 Assignment/src/cpd/pkg4414/assignment/Order.java
@@ -29,14 +29,9 @@ public class Order {
         this.notes = "";
     }
 
-    public Order(String customerId, String customerName, Date timeRecieved, Date timeProcessed, Date timeFullfilled, ArrayList<Purchase> purchases, String notes) {
+    public Order(String customerId, String customerName) {
         this.customerId = customerId;
         this.customerName = customerName;
-        this.timeRecieved = timeRecieved;
-        this.timeProcessed = timeProcessed;
-        this.timeFullfilled = timeFullfilled;
-        this.purchases = purchases;
-        this.notes = notes;
     }
 
     public String getCustomerId() {
