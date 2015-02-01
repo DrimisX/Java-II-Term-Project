c0a12df71752d7b1003e2d16d690b9401168e751
Dylan <dylan@dylanweb.ca>
2015-01-31 19:05:09 -0500

    Added JSONData StringBuilder for getReport() method of OrderQueue Class

--- CPD-4414 Assignment/src/cpd/pkg4414/assignment/OrderQueue.java
+++ CPD-4414 Assignment/src/cpd/pkg4414/assignment/OrderQueue.java
@@ -33,6 +33,66 @@ public class OrderQueue {
     public String getReport() {
         String jsonData = ""; //Parse JSON Data into this string
         
+        //IF THERE ARE ORDERS IN THE ARRAYDEQUEUE
+        if (orderQueue.size()>0) {
+            
+            //BEGIN BUILDING STRING
+            jsonData = new StringBuilder()
+                    
+                    //OPENING DELIMITER FOR THE JSONDATA STRING
+                    .append("{ \"orders\" : [\n").toString();
+            
+                    //ITERATE THROUGH ORDERS FOR ORDERQUEUE
+                    for(Iterator itr = orderQueue.iterator();orderQueue.hasNext();)  {
+                        
+                        //System.out.println(itr.next()); //Comment example of simple iteration output (for reference)
+                        
+                        //APPEND THE BASIC ORDER DETAILS
+                        jsonData.append("\t{ \"customerId\" : \"" + itr.getCustomerId() + "\",\n")
+                            .append("\t\t\"customerName\" : \"" + itr.getCustomerName + "\",\n")
+                            .append("\t\t\"timeReceived\" : \"" + itr.getTimeRecieved + "\",\n")
+                            .append("\t\t\"timeProcessed\" : \"" + itr.getTimeProcessed + "\",\n")
+                            .append("\t\t\"timeFulfilled\" : \"" + itr.getTimeFullfilled + "\",\n")
+                            .append("\t\t\"purchases\" : [\n");
+                        
+                            //ITERATE THROUGH PURCHASES FOR EACH ORDER
+                            for(Iterator pItr = orderQueue.getPurchases().iterator();orderQueue.getPurchases().hasNext();)  {
+                                //APPEND PURCHASE ID AND QUANTITY
+                                jsonData.append("\t\t\t{ \"productId\" : " + pItr.getProductId 
+                                        + ", \"quantity\" : " + pItr.getQuantity + " }");
+                                
+                                //IF THERE IS ANOTHER PURCHASE, ADD COMMA TO SEPARATE PURCHASES
+                                if (orderQueue.getPurchases().hasNext())
+                                    jsonData.append(",\n");
+                                
+                                else
+                                //ADD NEWLINE AT END OF PURCHASE
+                                jsonData.append("\n");
+                            }
+                            
+                            //CLOSING DELIMITER FOR PURCHASES
+                            jsonData.append("\t\t],\n");
+                            
+                            //APPEND NOTES
+                            jsonData.append("\t\t\"notes\" : \"" + itr.getNotes + "\"\n");
+                        
+                        //CLOSING DELIMITER FOR ORDER
+                        jsonData.append("\t}");
+                            
+                        //IF THERE IS ANOTHER ORDER, ADD COMMA TO SEPARATE ORDERS
+                        if (orderQueue.hasNext())
+                           jsonData.append(",\n");
+                        
+                        else
+                        //ADD NEWLINE AT END OF ORDER    
+                        jsonData.append("\n");
+                    }
+                    
+                    //CLOSING DELIMITER FOR JSONDATA
+                    jsonData.append("] }\n");
+
+        }
+        
         return jsonData;
     }
 }
